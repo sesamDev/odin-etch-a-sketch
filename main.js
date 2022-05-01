556132f10b0e6415cb656445f441fabd657c52e8
Sebastian <sebastian.samils@gmail.com>
2022-05-01 10:14:10 +0200

    Added Generate grid button
    The button clears the current grid with function removeCurrentGrid.
    It also creates a new one based on the users input.

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-let gridWidth = 16; //Could use one variable for both width and height, this is easier to read though.
-let gridHeight = 16;
+let gridWidth = 2; //Could use one variable for both width and height, this is easier to read though.
+let gridHeight = 2;
 let cellSize = 50;
-let grid = [gridWidth,gridHeight];
+//let grid = [gridWidth,gridHeight];
 const gridContainer = document.querySelector('#grid-container');
 let setGridSize = ''
+const generateGridButton = document.querySelector('button'); //Grab a reference to "Generate grid" button
 
 //Create the divs which makes up the grid.
-for (let x = 0; x < grid[0]; x++) {
-    for (let y = 0; y < grid[1]; y++) {
-        let gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridItem.textContent = `${x}, ${y}`;
-        gridContainer.appendChild(gridItem);
+function createGrid(width, height){
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            let gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.textContent = `${x}, ${y}`;
+            gridContainer.appendChild(gridItem);
+        }
     }
+    //Arrange the grid to be w*h
+    for (let i = 0; i < width; i++) {
+        setGridSize += `${cellSize}px `;
+    }
+    gridContainer.style.gridTemplateColumns = setGridSize;
+    gridContainer.style.gridTemplateRows = setGridSize;
 }
 
-//Arrange the grid to be w*h
-for (let i = 0; i < gridHeight; i++) {
-    setGridSize += `${cellSize}px `;
+function removeCurrentGrid(){
+    while(gridContainer.hasChildNodes()){
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    setGridSize = ''; //reset number of rows and columns
 }
-gridContainer.style.gridTemplateColumns = setGridSize;
-gridContainer.style.gridTemplateRows = setGridSize;
+
+//Instantiate default grid on page load.
+createGrid(gridWidth, gridHeight);
+
 
 //Attach a hover event listner to the grid
 gridContainer.addEventListener('mouseover', function(e){
@@ -31,8 +45,15 @@ gridContainer.addEventListener('mouseover', function(e){
     console.log(e.target.classList);
 })
 
+//Generate new grid after pressing "Generate grid" button
+let gridSizeInput = '';
+generateGridButton.addEventListener('click', () =>{
+    gridSizeInput = prompt("How many squares do you want?") //Ask for the number squares per side for the new grid.
+    removeCurrentGrid(); //Remove the current grid.
+    createGrid(gridSizeInput,gridSizeInput); //Create a new grid based on user input.
+})
 function test(){
-    console.log(setGridSize);
+    console.log(gridContainer.hasChildNodes());
 }
 
 test();
