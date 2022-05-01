bfab1659de464b329c32c0f4ea6034cd7a0c84bf
Sebastian <sebastian.samils@gmail.com>
2022-05-01 09:34:44 +0200

    Now able to change background when hovering over grid cell

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let gridWidth = 16; //Could use one variable for both width and height, this is easier to read though.
 let gridHeight = 16;
-let cellSize = 50; //Not in use yet, will probably need this later.
+let cellSize = 50;
 let grid = [gridWidth,gridHeight];
 const gridContainer = document.querySelector('#grid-container');
 let setGridSize = ''
@@ -13,18 +13,23 @@ for (let x = 0; x < grid[0]; x++) {
         gridItem.textContent = `${x}, ${y}`;
         gridContainer.appendChild(gridItem);
     }
-    
 }
 
 //Arrange the grid to be w*h
 for (let i = 0; i < gridHeight; i++) {
     setGridSize += `${cellSize}px `;
 }
-
 gridContainer.style.gridTemplateColumns = setGridSize;
 gridContainer.style.gridTemplateRows = setGridSize;
 
-
+//Attach a hover event listner to the grid
+gridContainer.addEventListener('mouseover', function(e){
+    if(e.target === 'grid-item'){
+    //
+    }
+    e.target.classList.add('hover');
+    console.log(e.target.classList);
+})
 
 function test(){
     console.log(setGridSize);
